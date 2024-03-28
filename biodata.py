40c4582abc8173ef180888dd8a1f523883e2a42c
Kemals3 <165116225+Kemals3@users.noreply.github.com>
2024-03-28 09:01:33 +0700

    Kamaludin
    Tugas Mata Kuliah Struktur Data
    Program Biodata sederhana (Python)

---
+++ biodata.py
@@ -0,0 +1,26 @@
+def run():
+    print('Pendataan Mahasiwa TA 2023/2024 ')
+    print('===============================>')
+    nama = input('input nama: ')
+    alamat = input('input alamat: ')
+    prodi = input('input Program Study: ')
+    semester = input('input Semester: ')
+    NIM = input('input Nomor Induk Mahasiwa: ')
+    pekerjaan = input('input pekerjaan: ')
+    
+    print('                     ')
+    print('                     ')
+    print('  Biodata Mahasiswa STMIK LOMBOK ')
+    print('<================================>')
+    print('nama: ' + nama)
+    print('alamat: ' + alamat)
+    print('prodi: ' + prodi)
+    print('semester: ' + semester)
+    print('NIM:d ' + NIM)
+    print('pekerjaan: ' + pekerjaan)
+    print('                     ')
+    print('Terimakasih Sudah Mengisi Data dan Good Luck')
+    
+ 
+if __name__ == '__main__':
+    run()
